79f269ee55dda0ec7842590b6c4646153d8b993a
Park Kyeong Jun <100195725+Kyeong6@users.noreply.github.com>
2024-09-20 20:32:51 +0900

    feat: query parameter_2 학습

--- 1_fastapi_request/main_query.py
+++ 1_fastapi_request/main_query.py
@@ -13,3 +13,34 @@ fake_item_db = [{"item_name" : "Foo"}, {"item_name" : "Bar"}, {"item_name" : "Ba
 async def read_item(skip: int = 0, limit: int = 10):
     # 인덱스
     return fake_item_db[skip : skip + limit]
+
+# 함수 인자값에 default 값이 주어지지 않으면 반드시 query parameter에 해당하는 인자가 주어져야 함
+@app.get("/items_id/")
+async def read_item_id(skip: int, limit: int):
+    return fake_item_db[skip : skip + limit]
+
+# 함수 인자값에 default 값이 주어지지 않으면 None으로 설정
+# None이 있을 경우 조건문을 작성해주자 : None에 대한 처리
+# 추가적으로 Optional도 사용해보자!
+@app.get("/items_op/")
+async def read_item_op(skip: int, limit: Optional[int] = None):
+    if limit:
+       return fake_item_db[skip : skip + limit]
+    else:
+        return {"limit is not privided"}
+    
+# Path와 Query Parameter에 함께 사용
+# decorator operator에서 자동으로 item_id를 Path Parameter로 인지
+# 수행 함수 인자 중 item_id는 Path Parameter, 나머지는 모두 Query Paramter
+# query parameter는 q=값 이런 형식으로 url 구성필요 
+# bool 데이터 형식은 0 or 1로 작성
+@app.get("/items/{item_id}")
+async def read_item_path_query(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id" : item_id}
+    if q:
+        item.update({"q" : q})
+    if not short:
+        item.update(
+            {"description" : "This is an amazing item that has a long description"}
+        )
+    return item
